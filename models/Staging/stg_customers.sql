4d71c09354db881cf610d12b041e87c28606af3a
Ramón Sánchez Quijada <chimpin3000@hotmail.es>
2024-05-29 10:31:55 +0000

    added is_incremental

--- models/Staging/stg_customers.sql
+++ models/Staging/stg_customers.sql
@@ -14,4 +14,11 @@ select
 from {{ source('stg_tables_tpch_sf1', 'customer') }}
 order by customer_id asc
 
+-- Para hacerlo incremental
+{% if is_incremental() %}
+    where customer_id > (select (max(customer_id)) from {{ this }})
+{% endif %}
+--{{ this }} para referirnos a la tabla que estamos creando/actualizando
 
+-- básicamente esto hace lo mismo que mi macro de delete, pero de esta manera comprobamos que el último customer_id sea más pequeño que lo nuevo que estamos metiendo
+-- para de esta manera que no haya duplicados y no se me vuelvan a cargar los datos antiguos. Hay que hacerlo para todas las incrementales, y digamos que es obligatorio
